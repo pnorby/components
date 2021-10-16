3fa0ae50b4577231ca4d3a4f905aa21a7ffb8a6a
Jeri <79335135+jeriaca@users.noreply.github.com>
2021-10-16 14:31:34 -0500

    Generated my component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { JacComponentComponent } from './jac-component/jac-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PizzaToppingsComponent
+    PizzaToppingsComponent,
+    JacComponentComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/jac-component/jac-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jac-component',
+  templateUrl: './jac-component.component.html',
+  styleUrls: ['./jac-component.component.css']
+})
+export class JacComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
