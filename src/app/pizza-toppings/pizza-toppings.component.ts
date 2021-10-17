31b06d6d543b344b3a76b763eb13b7865c807a0d
sp323 <patel.sujal.r@gmail.com>
2021-10-16 20:55:48 -0500

    Finalize Week 6 work

--- src/app/pizza-toppings/pizza-toppings.component.ts
+++ src/app/pizza-toppings/pizza-toppings.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { PizzaService } from "../pizza.service";
 
+
+interface PizzaToppingDisplay {
+  name: string;
+  price: number;
+  checked: boolean;
+}
+
 @Component({
   selector: 'app-pizza-toppings',
   templateUrl: './pizza-toppings.component.html',
@@ -9,15 +16,29 @@ import { PizzaService } from "../pizza.service";
 export class PizzaToppingsComponent implements OnInit {
 
 
-    //
+  //
   constructor(
-      private pizzaSvc: PizzaService
+    private pizzaSvc: PizzaService
   ) { }
 
+  availablePizzaToppings: PizzaToppingDisplay[] = [];
+
   ngOnInit(): void {
-      const pt = this.pizzaSvc.getPizzaToppings();
-      console.log(pt);
 
+    const pt = this.pizzaSvc.getPizzaToppings();
+    console.log(pt);
+
+    this.availablePizzaToppings = pt.map(
+      x => ({
+        ...x,
+        checked: false
+      })
+    );
+
+    console.log(this.availablePizzaToppings);
   }
-
+  totalPrice = 0;
+  calculateTotal = () => {
+      this.totalPrice = this.availablePizzaToppings.filter(x => x.checked).reduce((acc, x) => acc + x.price, 0);
+  }
 }
