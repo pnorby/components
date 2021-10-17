84ecaefd57ad29358773d1dff66e6e5bc4cfb8fd
tsteelemadisoncollege <tsteele@madisoncollege.edu>
2021-10-17 11:05:07 -0500

    Fixe duplicate imports...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,6 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { TjsAdditionComponent } from './tjs-addition/tjs-addition.component';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-
 import { TjsMultiplicationComponent } from './tjs-multiplication/tjs-multiplication.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
