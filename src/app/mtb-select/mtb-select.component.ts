76c3ab6296483a490fd6424f224c6376723821c3
Matt TerBeek <matthewevansterbeek@gmail.com>
2021-10-17 21:59:11 -0500

    Added quote of the day component

---
+++ src/app/mtb-select/mtb-select.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mtb-select',
+  templateUrl: './mtb-select.component.html',
+  styleUrls: ['./mtb-select.component.css']
+})
+export class MtbSelectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+
+}
+
